58b79d08d5c6d3892285b190e89b333c6dd5215f
nitesh sharma <nitesh.grep@gmail.com>
2014-05-28 19:49:07 +0530

    using controller class

--- mvc/controller.js
+++ mvc/controller.js
@@ -21,3 +21,19 @@
 
 })(jQuery, window);
 
+
+(function($, Controller){
+
+	var mod = new Controller();
+
+	mod.toggleClass = function(e){
+		this.view.toggleClass("over", e.data);
+	};
+
+	mod.load(function(){
+		this.view = $("#view");
+		this.view.mouseover(this.proxy(this.toggleClass), true);
+		this.view.mouseout(this.proxy(this.toggleClass), false);
+	});
+
+})(jQuery, Controller);
