1657aea3dcc1cebff2715f223afde8b1ce89d9d8
nitesh sharma <nitesh.grep@gmail.com>
2014-05-29 16:55:11 +0530

    refactoring controller class

--- mvc/controller.js
+++ mvc/controller.js
@@ -1,23 +1,62 @@
 var exports = this;
 
-(function(){
+(function() {
 	var mod = {};
 
-	mod.create = function(includes){
-		var result = function(){
+	mod.create = function(includes) {
+		var result = function() {
 			this.init.apply(this, arguments);
 		};
 
 		result.fn = result.prototype;
-		result.fn.init = function(){};
+		result.fn.init = function() {};
 
-		result.proxy = function(func){ return $.proxy(func, this); };
+		result.proxy = function(func) {
+			return $.proxy(func, this);
+		};
 		result.fn.proxy = result.proxy;
 
-		result.include = function(obj) { $.extend(this.fn, obj);};
-		result.extend = function(obj) { $.extend(this, obj);};
-		
-		if(includes) result.include(includes);
+		result.include = function(obj) {
+			$.extend(this.fn, obj);
+		};
+		result.extend = function(obj) {
+			$.extend(this, obj);
+		};
+
+		result.include({
+			eventSplitter: /^(\w+)\s*(.*)$/,
+
+			delegateEvents: function() {
+				for (var key in this.events) {
+					var methodName = this.events[key];
+					var method = this.proxy(this[methodName]);
+
+					var match = key.match(this.eventSplitter);
+					var eventName = match[1];
+					var selector = match[2];
+
+					if (selector === "") {
+						this.el.bind(eventName, method);
+					} else {
+						this.el.delegate(selector, eventName, method);
+					}
+
+				}
+			},
+
+			$: function(selector) {
+				return $(selector, this.el);
+			},
+
+			refereshElements: function() {
+				for (var key in this.elements) {
+					this[this.elements[key]] = this.$(key);
+				}
+			}
+		})
+
+
+		if (includes) result.include(includes);
 
 		return result;
 	};
@@ -26,29 +65,19 @@ var exports = this;
 
 })(jQuery);
 
-$(function($){
+$(function($) {
 	var ToggleView = Controller.create({
-		init: function(element){
+		init: function(element) {
 			this.el = $(element);
 			this.refereshElements();
 			this.searchForm.submit(this.proxy(this.search));
 		},
 
-		search: function(){
+		search: function() {
 
 		},
 
-		$: function(selector){
-			return $(selector, this.el);
-		},
-
-		refereshElements: function(){
-			for(var key in this.elements){
-				this[this.elements[key]] = this.$(key);
-			}
-		},
-
-		toggleClass: function(e){
+		toggleClass: function(e) {
 			this.view.toggleClass("over", e.data);
 		},
 
@@ -57,6 +86,9 @@ $(function($){
 			"form input[type=text]": "searchInput"
 		},
 
+		events: {
+			"submit form": "submit"
+		}
 
 	});
 
